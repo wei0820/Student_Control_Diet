df6e28aa2425694f6be63ba0a9eb3c28945ce5ca
Wei Wei <jheng.wei0919@gmail.com>
2020-04-23 10:13:43 +0800

    first commit

---
+++ app/src/main/java/com/student/student_searchmap/MainActivity.kt
@@ -0,0 +1,210 @@
+package com.student.student_searchmap
+
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.location.Geocoder
+import android.media.Image
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.format.DateFormat
+import android.util.Base64
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.maps.model.LatLng
+import com.google.gson.Gson
+import com.hendraanggrian.pikasso.picasso
+import com.jackpan.libs.mfirebaselib.MfiebaselibsClass
+import com.jackpan.libs.mfirebaselib.MfirebaeCallback
+import com.student.student_searchmap.Data.ResponseData
+import java.util.*
+import com.firebase.client.Firebase
+
+
+
+class MainActivity : AppCompatActivity(), MfirebaeCallback {
+    override fun getUserLogoutState(p0: Boolean) {
+    }
+
+    override fun resetPassWordState(p0: Boolean) {
+    }
+
+    override fun getsSndPasswordResetEmailState(p0: Boolean) {
+    }
+
+    override fun getFirebaseStorageType(p0: String?, p1: String?) {
+    }
+
+    override fun getUpdateUserName(p0: Boolean) {
+    }
+
+    override fun getDatabaseData(p0: Any?) {
+
+
+    }
+
+    override fun getuserLoginEmail(p0: String?) {
+    }
+
+    override fun getDeleteState(p0: Boolean, p1: String?, p2: Any?) {
+        if (p0) {
+            Toast.makeText(this, "刪除成功！", Toast.LENGTH_SHORT).show();
+            mFirebselibClass.getFirebaseDatabase(ResponseData.KEY_URL, "data")
+        } else {
+            Toast.makeText(this, "刪除失敗！", Toast.LENGTH_SHORT).show();
+        }    }
+
+    override fun getFireBaseDBState(p0: Boolean, p1: String?) {
+    }
+
+    override fun getuseLoginId(p0: String?) {
+    }
+
+    override fun createUserState(p0: Boolean) {
+    }
+
+    override fun useLognState(p0: Boolean) {
+    }
+
+    override fun getFirebaseStorageState(p0: Boolean) {
+    }
+    lateinit var img :ImageView
+    lateinit var add :TextView
+    lateinit var select:TextView
+    lateinit var phone :TextView
+    lateinit var price :TextView
+    lateinit var time :TextView
+    lateinit var message :TextView
+    lateinit var button: Button
+    lateinit var mProgressDialog : ProgressDialog
+    lateinit var mFirebselibClass: MfiebaselibsClass
+    lateinit var mLatBtn :Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContentView(R.layout.activity_main)
+        img = findViewById(R.id.img)
+        add  =findViewById(R.id.add)
+        select = findViewById(R.id.select)
+        phone = findViewById(R.id.phone)
+        price = findViewById(R.id.price)
+        time =findViewById(R.id.time)
+        message = findViewById(R.id.message)
+        button = findViewById(R.id.button)
+        mLatBtn = findViewById(R.id.lotbtn)
+
+
+//        mProgressDialog = ProgressDialog(this)
+//
+//        mProgressDialog.setMessage("loading")
+//        mProgressDialog.setCancelable(false)
+//        mProgressDialog.show()
+        getData()
+
+    }
+    fun  getData(){
+        val jsonString: String = intent.extras.getString("json")
+        val type: String = intent.extras.getString("type")
+        var lat :Double = intent.extras.getDouble("lat",0.0)
+        var lon :Double = intent.extras.getDouble("lon",0.0)
+
+        val gson =Gson()
+        val responseData = gson.fromJson(jsonString,ResponseData::class.java)
+        if (responseData.url!=null){
+            decode(responseData.url)
+
+        }
+        Log.d("Jack",ResponseData.KEY_URL+type)
+        select.text = responseData.select
+        phone.text = responseData.phone
+        price.text =responseData.price
+        time.text = "開始:"+ responseData.starttime+"~"+"結束:"+ responseData.endtime
+        message.text = responseData.message
+        handleLocation(responseData.lat.toDouble(),responseData.lon.toDouble(),add)
+
+        button.setOnClickListener {
+            Toast.makeText(this,"預約成功",Toast.LENGTH_SHORT).show()
+
+            val phone :String = responseData.phone.replaceFirst("0","+886")
+            val firebase = Firebase(ResponseData.KEY_URL+type)
+            firebase.child(responseData.date).removeValue();
+            val mCal = Calendar.getInstance()
+            val s = DateFormat.format("yyyy-MM-dd kk:mm:ss", mCal.getTime());
+            sendSMS(phone,s.toString(),responseData.id)
+        }
+
+        mLatBtn.setOnClickListener {
+            googlemap(lat,lon,responseData.lat.toDouble(),responseData.lon.toDouble())
+        }
+
+
+    }
+    fun decode(imageString: String) {
+
+        //decode base64 string to image
+        val imageBytes = Base64.decode(imageString, Base64.DEFAULT)
+        val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+
+        img.setImageBitmap(decodedImage)
+    }
+    private fun handleLocation(latitude: Double,longitude:Double,textView: TextView)  {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        Thread(Runnable {
+            try {
+                var addresses = geocoder.getFromLocation(
+                        latitude, longitude, 1
+                )
+                textView.text = addresses.get(0).getAddressLine(0)
+//                mProgressDialog.dismiss()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }).start()
+    }
+
+    protected fun sendSMS(phone:String,time:String,id:String) {
+
+        val smsIntent = Intent(Intent.ACTION_VIEW)
+        smsIntent.data = Uri.parse("smsto:")
+        smsIntent.type = "vnd.android-dir/mms-sms"
+
+        smsIntent.putExtra("address", phone)
+        smsIntent.putExtra("sms_body", "親愛的客戶您好:謝謝您使用停車App於"+time+"預租停車位,如有疑義,請洽詢客服信箱 ya_1827@yahoo.com.tw")
+        try {
+            startActivity(smsIntent)
+            finish()
+            Log.i("Finished sending SMS...", "")
+        } catch (ex: android.content.ActivityNotFoundException) {
+            Toast.makeText(this,
+                    "SMS faild, please try again later.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    fun googlemap(startLatitude :Double,startLongitude:Double,endLatitude:Double,endLongitude:Double){
+        val startLatitude = startLatitude
+        val startLongitude = startLongitude
+
+        val endLatitude = endLatitude
+        val endLongitude = endLongitude
+
+        val saddr = "saddr=$startLatitude,$startLongitude"
+        val daddr = "daddr=$endLatitude,$endLongitude"
+        val uriString = "http://maps.google.com/maps?$saddr&$daddr"
+
+        val uri = Uri.parse(uriString)
+
+        val intent = Intent(android.content.Intent.ACTION_VIEW, uri)
+
+        // If you want to get rid of the dialog,
+        // Before the startActivity() add this
+        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity")
+
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/student/student_searchmap/PlaceDeatilActivity.kt
@@ -0,0 +1,213 @@
+package com.student.student_searchmap
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RatingBar
+import android.widget.TextView
+import com.hendraanggrian.pikasso.picasso
+import com.student.student_searchmap.Data.GoogleMapPlaceDetailsData
+import com.student.student_searchmap.Data.GoogleResponseData
+import android.widget.Toast
+import android.content.Intent
+import android.net.Uri
+
+
+class PlaceDeatilActivity : AppCompatActivity(), GoogleMapAPISerive.GetResponse {
+    override fun getData(googleResponseData: GoogleResponseData?) {
+
+    }
+
+    override fun getDetailData(googleMapPlaceDetailsData: GoogleMapPlaceDetailsData?) {
+        if (googleMapPlaceDetailsData != null) {
+            if (googleMapPlaceDetailsData.result != null) {
+
+                if (googleMapPlaceDetailsData.result.photos != null && googleMapPlaceDetailsData.result.photos.size != 0) {
+
+                }
+                mRatingBar.numStars = 5
+                mRatingBar.rating = googleMapPlaceDetailsData.result.rating
+
+                mNameText.text = "名稱：" + googleMapPlaceDetailsData.result.name
+                mAddressText.text = "地址：" + googleMapPlaceDetailsData.result.formatted_address
+                mAddressText.setOnClickListener {
+                    googlemap(lat,lon,googleMapPlaceDetailsData.result.geometry.location.lat,googleMapPlaceDetailsData.result.geometry.location.lng)
+                }
+                if (googleMapPlaceDetailsData.result.formatted_phone_number!=null&&!googleMapPlaceDetailsData.result.formatted_phone_number.equals("null")){
+                    mPhoneText.text = "聯絡電話：" + googleMapPlaceDetailsData.result.formatted_phone_number
+
+
+                }else{
+                    mPhoneText.text = "尚未提供電話"
+
+                }
+                if (googleMapPlaceDetailsData.result.opening_hours != null) {
+                    if (googleMapPlaceDetailsData.result.opening_hours.open_now) {
+                        mOpenNowText.text = "目前營業中!"
+                    } else {
+                        mOpenNowText.text = "目前沒有營業!"
+
+                    }
+                }
+
+                if (googleMapPlaceDetailsData.result.opening_hours != null && googleMapPlaceDetailsData.result.opening_hours.weekday_text != null) {
+                    mOPenText.text = "營業時間：" + "\n" + googleMapPlaceDetailsData.result.opening_hours.weekday_text[0] + "\n" + googleMapPlaceDetailsData.result.opening_hours.weekday_text[1] + "\n" + googleMapPlaceDetailsData.result.opening_hours.weekday_text[2] + "\n" +
+                            googleMapPlaceDetailsData.result.opening_hours.weekday_text[3] + "\n" + googleMapPlaceDetailsData.result.opening_hours.weekday_text[4] + "\n" + googleMapPlaceDetailsData.result.opening_hours.weekday_text[5] + "\n" + googleMapPlaceDetailsData.result.opening_hours.weekday_text[6]
+
+                }
+
+                if (googleMapPlaceDetailsData.result.photos != null) {
+                    for (photos in googleMapPlaceDetailsData.result.photos) {
+                        var photoString: String = GoogleMapAPISerive.getPhotos(this, photos.photo_reference)
+                        mPhotoData.add(photoString)
+
+
+                    }
+                }else{
+                    mPhotoData.add(GoogleMapAPISerive.getPhotos(this,url))
+                }
+                mImagePagerAdapter.notifyDataSetChanged()
+
+                if (googleMapPlaceDetailsData.result.reviews != null) {
+                    for (review in googleMapPlaceDetailsData.result.reviews) {
+                        addnewLayout(review)
+
+
+                    }
+
+
+                }
+
+
+            }
+        }
+    }
+    lateinit var mReViewListView: LinearLayout
+    lateinit var mViewPage: ViewPager
+    lateinit var mNameText: TextView
+    lateinit var mAddressText: TextView
+    lateinit var mPhoneText: TextView
+    lateinit var mOpenNowText: TextView
+    lateinit var mOPenText: TextView
+    lateinit var mRatingBar: RatingBar
+    var url :String = ""
+    var mPhotoData: ArrayList<String> = ArrayList()
+    lateinit var mImagePagerAdapter: ImagePagerAdapter
+     var lat : Double = 0.0
+    var lon  : Double = 0.0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_place_deatil)
+        getid()
+        initlayout()
+    }
+    fun initlayout(){
+        mViewPage = findViewById(R.id.viewpage)
+        mImagePagerAdapter = ImagePagerAdapter(this, mPhotoData)
+        mViewPage.adapter = mImagePagerAdapter
+        mNameText = findViewById(R.id.nametext)
+        mAddressText = findViewById(R.id.addresstext)
+        mPhoneText = findViewById(R.id.phonetext)
+        mOpenNowText = findViewById(R.id.opennowtext)
+        mOPenText = findViewById(R.id.opentext)
+        mRatingBar = findViewById(R.id.rating)
+        mReViewListView = findViewById(R.id.reviewlistview)
+
+
+    }
+
+    fun getid() {
+        val id: String = intent.extras.getString("id")
+        url = intent.extras.getString("tag")
+        lat =  intent.extras.getDouble("lat")
+        lon =  intent.extras.getDouble("lon")
+        GoogleMapAPISerive.getPlaceDeatail(this, id, this)
+
+
+    }
+    fun addnewLayout(data: GoogleMapPlaceDetailsData.Result.Reviews) {
+        val view = getLayoutInflater().inflate(R.layout.review_layout, null)
+        var mReviewImg: ImageView = view!!.findViewById(R.id.reviewimg)
+        var mReviewName: TextView = view!!.findViewById(R.id.reviewname)
+        var mReviewText: TextView = view!!.findViewById(R.id.reviewtext)
+        var mReviewTime: TextView = view!!.findViewById(R.id.reviewtimetext)
+        var mReviewRating: RatingBar = view!!.findViewById(R.id.rating)
+
+        if (data.profile_photo_url != null) {
+            picasso.load(data.profile_photo_url).error(R.mipmap.ic_launcher).into(mReviewImg)
+        }
+        mReviewName.text = data.author_name
+        mReviewText.text = data.text
+        mReviewRating.rating = data.rating
+        mReViewListView.addView(view)
+
+
+    }
+    inner class ImagePagerAdapter(internal var context: Context, internal var arrayList: ArrayList<String>?) : PagerAdapter() {
+        internal var layoutInflater: LayoutInflater
+
+        init {
+            layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        }
+
+        override fun getCount(): Int {
+            return if (arrayList != null) {
+                arrayList!!.size
+            } else 0
+        }
+
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            return view === `object` as LinearLayout
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val itemView = layoutInflater.inflate(R.layout.image_viewpager_layout, container, false)
+            val imageView = itemView.findViewById<ImageView>(R.id.viewPagerItem_image1)
+            picasso.load(arrayList!![position]).error(R.mipmap.ic_launcher_round).into(imageView)
+            container.addView(itemView)
+
+            return itemView
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            container.removeView(`object` as LinearLayout)
+        }
+
+    }
+
+    fun Call(s:String) {
+        val myIntentDial = Intent(Intent.ACTION_CALL, Uri.parse("tel:$s"))
+        startActivity(myIntentDial)
+
+    }
+    fun googlemap(startLatitude :Double,startLongitude:Double,endLatitude:Double,endLongitude:Double){
+        val startLatitude = startLatitude
+        val startLongitude = startLongitude
+
+        val endLatitude = endLatitude
+        val endLongitude = endLongitude
+
+        val saddr = "saddr=$startLatitude,$startLongitude"
+        val daddr = "daddr=$endLatitude,$endLongitude"
+        val uriString = "http://maps.google.com/maps?$saddr&$daddr"
+
+        val uri = Uri.parse(uriString)
+
+        val intent = Intent(android.content.Intent.ACTION_VIEW, uri)
+
+        // If you want to get rid of the dialog,
+        // Before the startActivity() add this
+        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity")
+
+        startActivity(intent)
+    }
+}
