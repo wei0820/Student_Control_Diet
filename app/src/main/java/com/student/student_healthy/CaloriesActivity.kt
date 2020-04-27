0ecac71bf30bfe22b78d410c46cb5294f074336b
Wei Wei <jheng.wei0919@gmail.com>
2020-04-27 18:09:50 +0800

    fix layout

--- app/src/main/java/com/student/student_healthy/CaloriesActivity.kt
+++ app/src/main/java/com/student/student_healthy/CaloriesActivity.kt
@@ -1,11 +1,14 @@
 package com.student.student_healthy
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import com.student.student_searchmap.R
 
@@ -15,7 +18,9 @@ class CaloriesActivity : AppCompatActivity() {
     lateinit var mButton3: Button
     lateinit var mButton4: Button
     lateinit var mAddButton: Button
+    lateinit var mHaveDateLayout:RelativeLayout
 
+    lateinit var mNoDateLayout:RelativeLayout
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,15 +29,10 @@ class CaloriesActivity : AppCompatActivity() {
         initLayout()
 
     }
-    fun setData(title :String){
-        var intent = Intent(KotlinActivity@this, Main2Activity::class.java)
-        var bundle = Bundle()
-        bundle.putString("title",title)
-        intent.putExtras(bundle)
-        startActivityForResult(intent, 0)
-
-    }
     fun initLayout(){
+        mHaveDateLayout = findViewById(R.id.havelayout)
+        mNoDateLayout = findViewById(R.id.nodatelayout)
+
         mAddButton = findViewById(R.id.addbutton)
         mButton1 = findViewById(R.id.button1)
         mButton2 = findViewById(R.id.button2)
@@ -40,24 +40,32 @@ class CaloriesActivity : AppCompatActivity() {
         mButton4 = findViewById(R.id.button4)
         mButton1.setOnClickListener {
 //            setData("早餐")
+            mNoDateLayout.visibility = View.VISIBLE
+            mHaveDateLayout.visibility = View.GONE
+
         }
         mButton2.setOnClickListener {
 //            setData("午餐")
-
+            mNoDateLayout.visibility = View.GONE
+            mHaveDateLayout.visibility=View.VISIBLE
 
         }
         mButton3.setOnClickListener {
 //            setData("晚餐")
+            mNoDateLayout.visibility= View.VISIBLE
+            mHaveDateLayout.visibility=View.GONE
 
         }
         mButton4.setOnClickListener {
 //            setData("點心")
-
+            mNoDateLayout.visibility = View.GONE
+            mHaveDateLayout.visibility = View.VISIBLE
         }
         mAddButton.setOnClickListener {
             startActivity(Intent(this,Main2Activity::class.java))
 
         }
+
 
 
     }
